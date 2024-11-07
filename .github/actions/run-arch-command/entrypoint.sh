df9ed8ba5ab93250b78f82af2574e000f4792864
Giovanni Santini <giovannisantini93@yahoo.it>
2024-11-07 16:57:20 +0100

    fix: More build issues
    Ensure correct user build, avoid the Arch container as much as possible.

--- .github/actions/run-arch-command/entrypoint.sh
+++ .github/actions/run-arch-command/entrypoint.sh
@@ -13,6 +13,11 @@ fi
 mapfile -t INPUT_ARRAY <<< $INPUT_RUN
 for command in "${INPUT_ARRAY[@]}"
 do
+    if [ ! -n "$command" ]
+    then
+        continue
+    fi
+
     # Grouping guide here: https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
     echo "::group::Running the command '$command'..."
     $command
